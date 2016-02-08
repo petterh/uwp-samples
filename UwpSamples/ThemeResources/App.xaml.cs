ea71e87c0b6ec59477d6739bcb80f1a40cebc76f
Petter Hesselberg <petterh@microsoft.com>
2016-02-08 11:30:41 +0100

    Initial commit (ThemeResources)

---
+++ UwpSamples/ThemeResources/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿namespace ThemeResources
+{
+    using System;
+
+    using Windows.ApplicationModel;
+    using Windows.ApplicationModel.Activation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += (sender, e1) =>
+                    {
+                        throw new Exception("Failed to load Page " + e1.SourcePageType.FullName);
+                    };
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ UwpSamples/ThemeResources/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ThemeResources
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
